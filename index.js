38e59b29b17c9cd45df0bfaf5561ce0d24a7df1f
Siddhant Shukla <siddhantshukla356@gmail.com>
2019-04-29 15:17:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function whisper(string) {
 function logShout(string){
   console.log(string.toUpperCase())
 }
-function logShout(string){
+function logWhisper(string){
   
 }
 function sayHiToGrandma(string){
